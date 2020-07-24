f8d2ecce648cdf9653faf3e87ea2381f85bb2c49
Gina <ginal@indiefin.com>
2020-07-24 11:16:58 +0200

    Added inner text for btn

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,26 +1,28 @@
-let darkmode = localStorage.getItem('darkMode')
-const darkModeToggle = document.querySelector('#dark-mode-toggle')
+let darkmode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
 const enableDarkMode = () => {
-  document.body.classList.add('darkmode')
-  localStorage.setItem('darkMode', 'enabled')
-}
+	document.body.classList.add("darkmode");
+	localStorage.setItem("darkMode", "enabled");
+};
 
 const disableDarkMode = () => {
-  document.body.classList.remove('darkmode')
-  localStorage.setItem('darkMode', null)
+	document.body.classList.remove("darkmode");
+	localStorage.setItem("darkMode", null);
+};
+
+if (darkmode === "enabled") {
+	enableDarkMode();
 }
 
-if (darkmode === 'enabled') {
-  enableDarkMode()
-}
+darkModeToggle.addEventListener("click", () => {
+	darkmode = localStorage.getItem("darkMode");
 
-darkModeToggle.addEventListener('click', () => {
-  darkmode = localStorage.getItem('darkMode')
-
-  if (darkmode !== 'enabled') {
-    enableDarkMode()
-  } else {
-    disableDarkMode()
-  }
-})
+	if (darkmode !== "enabled") {
+		enableDarkMode();
+		darkModeToggle.innerHTML = "Enable dark mode";
+	} else {
+		disableDarkMode();
+		darkModeToggle.innerHTML = "Disable dark mode";
+	}
+});
